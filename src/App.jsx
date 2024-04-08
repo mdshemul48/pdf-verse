a5231eb1d252ab7e07eb81234d096d9623936bc1
MD. Shimul <mdshemul480@gmail.com>
2024-04-08 19:09:49 +0600

    adding project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { ViewAllPdfBook } from "./Components/PdfBooks/ViewAllPdfBook/ViewAllPdfBook.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { SideBar } from "./Components/Shared/SideBar/SideBar.jsx";
+
+function App() {
+  return (
+    <>
+      <main className="flex">
+        <SideBar />
+        <div className="ms-2 mt-2">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={ViewAllPdfBook} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/PdfBooks/ViewAllPdfBook/ViewAllPdfBook.jsx
@@ -0,0 +1,3 @@
+export const ViewAllPdfBook = () => {
+  return <div>ViewAllPdfBook</div>;
+};
